f8813bc23d3eac13d20d438244d6bf22940ec9e4
naziraliyevozodbek-dev <naziraliyevozodbek510@gmail.com>
2025-12-23 22:27:05 +0500

    Configure GitHub Pages and Actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/history-quiz-game/', // GitHub repozitoriy nomi bilan bir xil bo'lishi kerak
 })
